784fe94ed55c73298b90928cea41928490745eae
Junaid SIkander Hakro <Junaid_sikander@live.com>
2021-02-16 22:05:51 +0500

    [ADD] sub menues in services tab

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -7,6 +7,8 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import {makeStyles} from "@material-ui/styles";
 //Components
 import logo from '../../assets/logo.svg'
@@ -60,25 +62,37 @@ export default function (props) {
     const classes = useStyles();
 
     const [value, setValue] = useState(0);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [open, setOpen] = useState(false);
 
     const handleChange = (e, value) => {
         setValue(value);
     };
 
-    useEffect( () => {
-        if(window.location.pathname === '/' && value !== 0)
+    const handleClick = (e) => {
+        setAnchorEl(e.currentTarget);
+        setOpen(true);
+    };
+
+    const handleClose = (e) => {
+        setAnchorEl(null);
+        setOpen(false);
+    };
+
+    useEffect(() => {
+        if (window.location.pathname === '/' && value !== 0)
             setValue(0);
-        else if(window.location.pathname === '/services' && value !== 1)
+        else if (window.location.pathname === '/services' && value !== 1)
             setValue(1);
-        else if(window.location.pathname === '/revolution' && value !== 2)
+        else if (window.location.pathname === '/revolution' && value !== 2)
             setValue(2);
-        else if(window.location.pathname === '/about' && value !== 3)
+        else if (window.location.pathname === '/about' && value !== 3)
             setValue(3);
-        else if(window.location.pathname === '/contact' && value !== 4)
+        else if (window.location.pathname === '/contact' && value !== 4)
             setValue(4);
-        else if(window.location.pathname === '/estimate' && value !== 5)
+        else if (window.location.pathname === '/estimate' && value !== 5)
             setValue(5);
-    },[value]);
+    }, [value]);
 
     return (
         <>
@@ -99,14 +113,49 @@ export default function (props) {
                             onChange={handleChange}
                             indicatorColor="primary">
                             <Tab className={classes.tab} component={Link} to="/" label="Home"/>
-                            <Tab className={classes.tab} component={Link} to="/services" label="Services"/>
+                            <Tab
+                                aria-owns={anchorEl ? "simple-menu" : undefined}
+                                aria-haspopup={anchorEl ? "true" : undefined}
+                                className={classes.tab}
+                                component={Link}
+                                onMouseOver={event => handleClick(event)}
+                                to="/services"
+                                label="Services"/>
                             <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution"/>
                             <Tab className={classes.tab} component={Link} to="/about" label="About us"/>
                             <Tab className={classes.tab} component={Link} to="/contact" label="Contact us"/>
                         </Tabs>
-                        <Button className={classes.button} component={Link} to="/estimate" variant="contained" color="secondary">
+                        <Button className={classes.button} component={Link} to="/estimate" variant="contained"
+                                color="secondary">
                             Free Estimate
                         </Button>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            MenuListProps={{onMouseLeave: handleClose}}>
+                            <MenuItem onClick={() => {handleClose(); setValue(1)}}
+                                      component={Link}
+                                      to="services">
+                                Services
+                            </MenuItem>
+                            <MenuItem onClick={() => {handleClose(); setValue(1)}}
+                                      component={Link}
+                                      to="customsoftware">
+                                Custom Software Development
+                            </MenuItem>
+                            <MenuItem onClick={() => {handleClose(); setValue(1)}}
+                                      component={Link}
+                                      to="mobileapps">
+                                Mobile App Development
+                            </MenuItem>
+                            <MenuItem onClick={() => {handleClose(); setValue(1)}}
+                                      component={Link}
+                                      to="websites">
+                                Website Development
+                            </MenuItem>
+                        </Menu>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
