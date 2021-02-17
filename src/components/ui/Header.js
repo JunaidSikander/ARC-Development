b35761da057e27274633be2f790dd92a4ef9ca31
Junaid SIkander Hakro <Junaid_sikander@live.com>
2021-02-17 20:46:58 +0500

    [FIX] Refresh focus Tabs

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -106,19 +106,38 @@ export default function (props) {
     };
 
     useEffect(() => {
-        if (window.location.pathname === '/' && value !== 0)
-            setValue(0);
-        else if (window.location.pathname === '/services' && value !== 1)
-            setValue(1);
-        else if (window.location.pathname === '/revolution' && value !== 2)
-            setValue(2);
-        else if (window.location.pathname === '/about' && value !== 3)
-            setValue(3);
-        else if (window.location.pathname === '/contact' && value !== 4)
-            setValue(4);
-        else if (window.location.pathname === '/estimate' && value !== 5)
-            setValue(5);
-    }, [value]);
+        switch(window.location.pathname) {
+            case "/":
+                value !== 0 && setValue(0);
+                break;
+            case "/services":
+                value !== 1 && setValue(1) || setSelectedIndex(0);
+                break;
+            case "/customsoftware":
+                value !== 1 && setValue(1) || setSelectedIndex(1);
+                break;
+            case "/mobileapps":
+                value !== 1 && setValue(1) || setSelectedIndex(2);
+                break;
+            case "/websites":
+                value !== 1 && setValue(1) || setSelectedIndex(3);
+                break;
+            case "/revolution":
+                value !== 2 && setValue(2);
+                break;
+            case "/about":
+                value !== 3 && setValue(3);
+                break;
+            case "/contact":
+                value !== 4 && setValue(4);
+                break;
+            case "/estimate":
+                value !== 5 && setValue(5);
+                break;
+            default:
+                break;
+        }
+    }, [value, selectedIndex]);
 
     return (
         <>
@@ -165,7 +184,7 @@ export default function (props) {
                             elevation={0}>
 
                             {menuOptions.map( (option, i) => (
-                                <MenuItem key={option}
+                                <MenuItem key={i}
                                           classes={{ root: classes.menuItem}}
                                           component={Link}
                                           onClick={(e) => {handleMenuItemClick(e,i); setValue(1)}}
