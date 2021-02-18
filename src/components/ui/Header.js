b807906df029afd3464af4610a783813d38651d4
Junaid SIkander Hakro <Junaid_sikander@live.com>
2021-02-18 21:20:23 +0500

    [ADD] Drawer

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -12,8 +12,14 @@ import Button from "@material-ui/core/Button";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from '@material-ui/icons/Menu';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 //Components
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
     const {children, window} = props;
@@ -81,19 +87,45 @@ const useStyles = makeStyles(theme => ({
         '&:hover': {
             opacity: 1
         }
+    },
+    drawerIconContainer: {
+        marginLeft: 'auto',
+        '&:hover': {
+            backgroundColor: 'transparent'
+        }
+    },
+    drawerIcon: {
+        height: '40px',
+        width: '40px'
+    },
+    drawer: {
+        backgroundColor: theme.palette.common.blue
+    },
+    drawerItem: {
+        ...theme.typography.tab,
+        color: 'white',
+        opacity: 0.8
+    },
+    drawerItemEstimate: {
+        backgroundColor: theme.palette.common.orange
+    },
+    drawerItemSelected: {
+        opacity: 1
     }
 }));
 
 export default function (props) {
     const classes = useStyles();
     const theme = useTheme();
-
     const mediunScreen = useMediaQuery( theme.breakpoints.down('md'));
 
     const [value, setValue] = useState(0);
     const [anchorEl, setAnchorEl] = useState(null);
     const [openMenu, setOpenMenu] = useState(false);
+    const [openDrawer, setOpenDrawer] = useState(false);
     const [selectedIndex, setSelectedIndex] = useState(0);
+
+    const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
 
     const menuOptions = [
         {name: "Services", link: "/services"},
@@ -202,6 +234,66 @@ export default function (props) {
         </>
     );
 
+    const drawer = (
+        <>
+            <SwipeableDrawer classes={{ paper: classes.drawer }}
+                             disableBackdropTransition={!iOS}
+                             disableDiscovery={iOS}
+                             open={openDrawer}
+                             onClose={() => setOpenDrawer(false)}
+                             onOpen={() => setOpenDrawer(true)}>
+                <List disablePadding>
+                    <ListItem  className={value === 0 ? [classes.drawerItem, classes.drawerItemSelected]: classes.drawerItem}
+                               onClick={ () => {setOpenDrawer(false); setValue(0)} }
+                               selected={value === 0}
+                               divider button component={Link} to='/'>
+                        <ListItemText disableTypography>Home</ListItemText>
+                    </ListItem>
+
+                    <ListItem className={value === 1 ? [classes.drawerItem, classes.drawerItemSelected]: classes.drawerItem}
+                              onClick={ () => {setOpenDrawer(false); setValue(1)} }
+                              selected={value === 1}
+                              divider button component={Link} to='/services'>
+                        <ListItemText disableTypography>Services</ListItemText>
+                    </ListItem>
+
+                    <ListItem className={value === 2 ? [classes.drawerItem, classes.drawerItemSelected]: classes.drawerItem}
+                              onClick={ () => {setOpenDrawer(false); setValue(2)} }
+                              selected={value === 2}
+                              divider button component={Link} to='/revolution'>
+                        <ListItemText disableTypography>The Revolution</ListItemText>
+                    </ListItem>
+
+                    <ListItem className={value === 3 ? [classes.drawerItem, classes.drawerItemSelected]: classes.drawerItem}
+                              onClick={ () => {setOpenDrawer(false); setValue(3)} }
+                              selected={value === 3}
+                              divider button component={Link} to='/about'>
+                        <ListItemText disableTypography>About Us</ListItemText>
+                    </ListItem>
+
+                    <ListItem className={value === 4 ? [classes.drawerItem, classes.drawerItemSelected]: classes.drawerItem}
+                              onClick={ () => {setOpenDrawer(false); setValue(4)} }
+                              selected={value === 4}
+                              divider button component={Link} to='/contact'>
+                        <ListItemText disableTypography>Contact Us</ListItemText>
+                    </ListItem>
+
+                    <ListItem className={value === 5 ? [classes.drawerItem, classes.drawerItemSelected] : [classes.drawerItem, classes.drawerItemEstimate]}
+                              onClick={ () => {setOpenDrawer(false); setValue(5)} }
+                              selected={value === 5}
+                              divider button component={Link} to='/estimate'>
+                        <ListItemText disableTypography>Free Estimate</ListItemText>
+                    </ListItem>
+                </List>
+            </SwipeableDrawer>
+            <IconButton classes={{ root: classes.drawerIconContainer }}
+                        onClick={() => setOpenDrawer(!openDrawer)}
+                        disableRipple>
+                <MenuIcon className={classes.drawerIcon}/>
+            </IconButton>
+        </>
+    );
+
     return (
         <>
             <ElevationScroll>
@@ -215,7 +307,7 @@ export default function (props) {
                             onClick={() => setValue(0)}>
                             <img alt="company logo" className={classes.logo} src={logo}/>
                         </Button>
-                        { mediunScreen ? null : tabs }
+                        { mediunScreen ? drawer : tabs }
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
